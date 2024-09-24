af23b069c41ebdf502981f12d07ef77ba599975c
SangWon7242 <gotkdqja@gmail.com>
2024-09-24 23:58:36 +0900

    리액트 네이티브 코드 작성 규칙, 스타일 적용

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, StatusBar, View, Text} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello, JSX!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, JSX!</Text>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
 
 export default App;
