7aa58a07806a9f86be208272472c82bc81ff855b
SangWon7242 <gotkdqja@gmail.com>
2024-09-25 18:13:38 +0900

    FlexBox를 이용한 레이아웃

--- App.js
+++ App.js
@@ -1,37 +1,14 @@
-import React, {useState} from 'react';
-import {StyleSheet, StatusBar, View, Text, Button} from 'react-native';
+import React from 'react';
+import {View} from 'react-native';
 
 const App = () => {
-  const [number, setNumber] = useState(0);
-
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" />
-      <Text style={styles.text}>결과 : {number}</Text>
-      <View style={styles.btnGroup}>
-        <Button title="증가" onPress={() => setNumber(number + 1)} />
-        <Button title="감소" onPress={() => setNumber(number - 1)} />
-      </View>
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'darkorange'}}></View>
+      <View style={{flex: 3, backgroundColor: 'red'}}></View>
+      <View style={{flex: 1, backgroundColor: 'green'}}></View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  btnGroup: {
-    flexDirection: 'row',
-    gap: 10,
-  },
-});
 
 export default App;
