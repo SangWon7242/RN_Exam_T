1082b09bdc48df24b26daa614cef8e8c2f2481fc
SangWon7242 <gotkdqja@gmail.com>
2024-09-27 00:20:15 +0900

    날씨 앱 만들기 위한 스타일 적용

--- App.js
+++ App.js
@@ -1,14 +1,69 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: 'darkorange'}}></View>
-      <View style={{flex: 3, backgroundColor: 'red'}}></View>
-      <View style={{flex: 1, backgroundColor: 'green'}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Ansan</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.regDate}>Friday, 20, January</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.tempCon}>
+          <Text style={styles.temp}>31</Text>
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff42',
+  },
+  city: {
+    flex: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    color: 'black',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  regDate: {
+    borderRadius: 20,
+    backgroundColor: 'black',
+    color: '#ffff42',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  desc: {
+    marginTop: 10,
+    color: 'black',
+    fontSize: 17,
+  },
+  tempCon: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  temp: {
+    color: 'black',
+    fontSize: 173,
+  },
+});
 
 export default App;
