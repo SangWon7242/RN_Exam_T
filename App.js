50a7dfeb8e19e3a16c74c3f91aedaff14358e80e
SangWon7242 <gotkdqja@gmail.com>
2024-10-04 17:53:30 +0900

    구글 GeoLocation을 이용하여, 위도 경도 얻고, 좌표값을 통해 실제 주소 이름 얻어서 반영

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, ScrollView, Dimensions} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+import {GOOGLE_API_KEY} from '@env';
 
 /*
 const {width: SCREEN_WIDTH} = Dimensions.get('window');
@@ -10,10 +12,43 @@ console.log(SCREEN_WIDTH);
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const App = () => {
+  const [errorMsg, setErrorMsg] = useState(null); // 오류 메시지 상태
+  const [city, setCity] = useState('');
+
+  const myApiKey = GOOGLE_API_KEY;
+
+  const getAddressFromCoordinates = async ({latitude, longitude}) => {
+    const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${myApiKey}`;
+
+    try {
+      const response = await fetch(apiUrl); // API 요청
+      const data = await response.json(); // JSON 데이터로 변환
+
+      // console.log(data.results[5].formatted_address);
+      const cityAddress = data.results[5].formatted_address;
+      setCity(cityAddress);
+    } catch (error) {
+      setErrorMsg(error);
+      console.error('주소를 불러오는데 실패했습니다. :', error);
+      return 'Error fetching address';
+    }
+  };
+
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(position => {
+      const {latitude, longitude} = position.coords;
+      getAddressFromCoordinates({latitude, longitude});
+    });
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Ansan</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         horizontal
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // react-native-dotenv
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
